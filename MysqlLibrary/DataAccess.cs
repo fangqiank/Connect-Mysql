f8ab829463b89b8f339975afc5f7905769dae219
naiq f <fangqiank@outlook.com>
2022-03-15 08:51:29 +0800

    Init

---
+++ MysqlLibrary/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MysqlLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStr)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionStr))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+
+        public Task SaveData<T>(string sql, T parameters, string connectionStr)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionStr))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
---
+++ MysqlLibrary/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MysqlLibrary
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStr);
+        Task SaveData<T>(string sql, T parameters, string connectionStr);
+    }
+}
